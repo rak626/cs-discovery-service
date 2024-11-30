785ec0cd54642469c15172a17b54f9a1479a7341
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:27:08 +0530

    initial commit

---
+++ src/main/java/com/crowdseats/cs_discovery_service/CsDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crowdseats.cs_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crowdseats/cs_discovery_service/CsDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdseats.cs_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
